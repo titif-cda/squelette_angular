ad46403f3fc1ce3ffcb18be20028db7e7d16f3df
Abdellatif EL JID <abdellatif.eljid@sendoc.fr>
2025-05-02 18:05:53 +0200

    Modif accueil

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,5 +8,20 @@ import { CommonModule } from '@angular/common';
   templateUrl: './accueil.component.html',
   styleUrls: ['./accueil.component.css']
 })
-export class AccueilComponent {}
+export class AccueilComponent implements OnInit {
 
+  ngOnInit() {
+    // Fonction de détection du défilement pour l'animation des services
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  // Animation des éléments lors du défilement
+  handleScroll() {
+    const services = document.querySelector('.services');
+    const servicesPosition = services?.getBoundingClientRect().top || 0;
+    
+    if (servicesPosition <= window.innerHeight / 1.2) {
+      services?.classList.add('animate-services');
+    }
+  }
+}
